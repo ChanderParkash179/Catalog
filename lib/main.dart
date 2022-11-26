b3db76ad3b802040609cdde99945b716229fe0fd
Chander Parkash <chanderparkash179@gmail.com>
2022-11-26 15:11:13 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Text('Welcome to 30 days of Flutter'),
+        ),
+      ),
+    );
+  }
+}
