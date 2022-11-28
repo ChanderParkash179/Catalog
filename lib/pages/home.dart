56b837737da7f2276d5e4b87a5a0fc677750bb9e
Chander Parkash <chanderparkash179@gmail.com>
2022-11-28 22:59:17 +0500

    day13: JsonDecodingEncoding, initState(), loadingData

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,38 @@
 // ignore_for_file: prefer_const_constructors, unnecessary_string_interpolations, sort_child_properties_last
 
+import 'dart:convert';
+
 import 'package:catalog/models/catalog.dart';
 import 'package:catalog/widgets/drawer.dart';
 import 'package:catalog/widgets/item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../utils/routes.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   int days = 30;
+
   String courseName = 'Flutter';
   String title = 'Catalog';
   String homeImg = 'assets/imgs/homeImage.png';
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final catalogJson = await rootBundle.loadString("assets/json/catalog.json");
+    final decodeData = jsonDecode(catalogJson);
+    var productData = decodeData("producst");
+    print(productData);
+  }
 
   @override
   Widget build(BuildContext context) {
